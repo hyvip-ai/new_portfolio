2ff274dcce65a6c4d56ea03304cf15dbff87422f
rajatcdb <rajat@codebuddy.co>
2022-06-05 12:54:39 +0530

    Sttling Update

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,51 +1,51 @@
-import React, { useEffect } from "react";
-import classes from "../styles/intro.module.css";
-import { TimelineLite } from "gsap/dist/gsap";
+import React, { useEffect } from 'react';
+import classes from '../styles/intro.module.css';
+import { TimelineLite } from 'gsap/dist/gsap';
 function Intro() {
   useEffect(() => {
     const tl = new TimelineLite({
       paused: true,
       onComplete: () => {
         setTimeout(() => {
-          document.getElementById("intro").style.display = "none";
+          document.getElementById('intro').style.display = 'none';
         });
       },
     });
-    tl.fromTo(".animate", { y: 100 }, { y: 0, stagger: 0.2 })
+    tl.fromTo('.animate', { y: 100 }, { y: 0, stagger: 0.2 })
       .to(
-        ".animate",
+        '.animate',
         {
           y: -100,
           stagger: 0.2,
         },
-        "+=0.7"
+        '+=0.7'
       )
-      .to(".intro", { height: 0 }, "+=0.3");
+      .to('.intro', { height: 0 }, '+=0.3');
     tl.play();
   }, []);
   return (
-    <div className={`${classes.intro_outer} intro`} id="intro">
+    <div className={`${classes.intro_outer} intro`} id='intro'>
       <div className={classes.intro_inner}>
         <div className={classes.text}>
-          <h1 className="animate">while(alive) &#123;</h1>
+          <h1 className='animate'>while(alive) &#123;</h1>
         </div>
         <div className={classes.text}>
-          <h1 className="animate">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;eat();
+          <h1 className='animate'>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;eat(&nbsp;);
           </h1>
         </div>
         <div className={classes.text}>
-          <h1 className="animate">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sleep();
+          <h1 className='animate'>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sleep(&nbsp;);
           </h1>
         </div>
         <div className={classes.text}>
-          <h1 className="animate">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;code();
+          <h1 className='animate'>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;code(&nbsp;);
           </h1>
         </div>
         <div className={classes.text}>
-          <h1 className="animate">&#125;</h1>
+          <h1 className='animate'>&#125;</h1>
         </div>
       </div>
     </div>
